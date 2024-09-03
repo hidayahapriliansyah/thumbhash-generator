2f2bdef1ebe6db5278fdc7e512f9bd769368414b
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2024-09-04 06:58:04 +0700

    feat: add open graph detail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Thumbhash Generator by Adi Muhamad Firmansyah",
-  description: "Make beautiful thumbhash as small as possible",
+  description: "Effortless beautiful tiny image placeholder",
+  openGraph: {
+    title: 'Thumbhash Generator by Adi Muhamad Firmansyah',
+    description: 'Effortless beautiful tiny image placeholder',
+    url: 'https://thumbhash-generator.vercel.app',
+    siteName: 'Thumbhash Generator',
+    images: [
+      {
+        url: 'https://thumbhash-generator.vercel.app/og-image.png',
+        width: 1600,
+        height: 900,
+      },
+    ],
+    locale: 'id_ID',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
